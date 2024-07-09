4fe087a7735baff80ea0ab605f3620a754faa92b
millrnv <n.aguilar02@ufromail.cl>
2024-07-09 16:25:00 -0400

    relleno de clases y jsp

--- src/main/java/com/example/tallerRecuperativo/controller/RegistroClienteServlet.java
+++ src/main/java/com/example/tallerRecuperativo/controller/RegistroClienteServlet.java
@@ -1,4 +1,7 @@
 package com.example.tallerRecuperativo.controller;
 
 public class RegistroClienteServlet {
+
+
+
 }
--- src/main/java/com/example/tallerRecuperativo/controller/RegistroEntradaServlet.java
+++ src/main/java/com/example/tallerRecuperativo/controller/RegistroEntradaServlet.java
@@ -1,4 +1,17 @@
 package com.example.tallerRecuperativo.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class RegistroEntradaServlet {
+
+
+
+
+
+
 }
